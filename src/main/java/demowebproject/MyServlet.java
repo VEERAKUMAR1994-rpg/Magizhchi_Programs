6eb9fc0b5e62f9c5e65114ceee8aab2138bed2c6
VEERAKUMAR1994-rpgVEERAKUMAR1994-rpg <veerakumarv0305@gmail.com>
2026-01-09 11:03:07 +0530

    added

---
+++ src/main/java/demowebproject/MyServlet.java
@@ -0,0 +1,19 @@
+package demowebproject;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class MyServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("server called");
+	}
+
+}
